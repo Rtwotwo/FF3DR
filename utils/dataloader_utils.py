f6ff95414aa8614485b31f630e732cb62d12956b
Rtwotwo <744044292@qq.com>
2026-01-02 22:47:50 +0800

    Update dataset_utils.py

--- utils/dataloader_utils.py
+++ utils/dataloader_utils.py
@@ -6,67 +6,151 @@ Todo: 读取WHU-OMVS数据集的相机参数,深度图以及掩码等
 Homepage: https://github.com/Rtwotwo/FF3DR.git
 """
 import os
-import numpy as np
 import pyexr
+import numpy as np
+from typing import Dict, List,Tuple, Optional
 
 
-def read_camera_info(camera_info_path:str):
-    """读取WHU_OMVS数据集中每个场景的相机参数,包括
-    CAMERA_ID, WIDTH, HEIGHT, PIXELSIZE, PARAMS[fx,fy,cx,cy], DISTORTION[K1, K2, K3, P1, P2]
-    存储该文件的相对路径位于: WHU-OMVS/train/area1/info/camera_info.txt
-    camera_info_path输入相机的相关路径;Return返回存储五组相机视角的参数信息返回五组相机"""
-    with open(camera_info_path, 'r') as text_file:
-        camera_info_lines = text_file.readlines()
-    # 分割参数形成内外参数信息
-    CAMERAS_INFO = {}
-    for line in camera_info_lines[4:]:
-        camera_info = {}
-        line_data = list(map(float, line.split()))
-        camera_info['camera_id'] = int(line_data[0])
-        camera_info['image_size'] = line_data[1:3]
-        camera_info['pixel_size'] = line_data[3]
-        camera_info['f_xy'] = line_data[4:6]
-        camera_info['c_xy'] = line_data[6:8]
-        camera_info['k_123'] = line_data[8:11]
-        camera_info['p_12'] = line_data[11:13]
-        CAMERAS_INFO[f'{int(line_data[0])}'] = camera_info
-    print(f'[INFO] the camera_info path: {camera_info_path} has been read out! All cameras number: {len(CAMERAS_INFO)}')
-    return CAMERAS_INFO
+class Camera:
+    """注册相机内部参数包括id,尺寸,像素大小,焦距,主点,畸变参数等"""
+    def __init__(self,
+                camera_id: Optional[int] = None,
+                size: Optional[Tuple[int, int]] = None,               
+                pixelsize: Optional[float] = None,
+                focallength: Optional[Tuple[float, float]] = None,    
+                x0y0: Optional[Tuple[float, float]] = None,           
+                distortion: Optional[List[float]] = None              
+                ) -> None:
+        self.camera_id = camera_id
+        self.size = size # [width, height]
+        self.pixelsize = pixelsize        
+        self.focallength = focallength # [fx, fy]
+        self.x0y0 = x0y0 # [cx, cy]
+        self.distortion = distortion # [k1, k2, k3, p1, p2] (length=5)
+    def __lt__(self,):
+        return [self.camera_id, self.size, self.pixelsize, 
+                self.focallength, self.x0y0, self.distortion]
+    
+
+class Photo:
+    """注册图像的位姿参数包括id,相机id,旋转矩阵,投影中心,深度范围,名称等"""
+    def __init__(self,
+                image_id: Optional[int] = None,
+                camera_id: Optional[int] = None,
+                rotation_matrix: Optional[np.ndarray] = None,  
+                project_center: Optional[np.ndarray] = None,    
+                depth: Optional[Tuple[float, float]] = None,    
+                name: Optional[str] = None,                      
+                camera_coordinate_type: str = 'XrightYup',      
+                rotation_type: str = 'Rwc',                      
+                translation_type: str = 'twc'                    
+                ) -> None:
+        self.image_id = image_id
+        self.camera_id = camera_id
+        self.name = name
+        self.rotation_matrix = rotation_matrix  # Rwc [3,3]
+        self.project_center = project_center    # twc [x,y,z]
+        self.depth = depth                      # [mindepth, maxdepth]
+        self.camera_coordinate_type = camera_coordinate_type
+        self.rotation_type = rotation_type
+        self.translation_type = translation_type
+    def __lt__(self,):
+        return [self.image_id, self.camera_id,
+                self.rotation_matrix, self.project_center,
+                self.depth, self.name]
 
 
-def read_image_info(image_info_path:str):
-    """读取图片数据的位姿参数,主要包括:
-    IMAGE_ID, CAMERA_ID, Rwc[9], twc[3], MINDEPTH, MAXDEPTH, NAME
-    存储该文件的相对路径位于: WHU-OMVS/train/area1/info/image_info.txt
-    image_info_path存储图像参数的路径;Return包含图像位姿参数的字典"""
-    with open(image_info_path, 'r') as image_file:
-        image_info_data = image_file.readlines()
-    # 数据转换成字典形式
-    IMAGES_INFO = {}
-    for line in image_info_data[4:]:
-        parts = line.split()
-        line_data = list(map(float, parts[:16]))
-        img_name = str(parts[16])
-        IMAGES_INFO[img_name] = {
-            'image_id': int(line_data[0]),
-            'camera_id': int(line_data[1]),
-            'Rwc': line_data[2:11],              
-            'twc': line_data[11:14],             
-            'min_depth': line_data[14],
-            'max_depth': line_data[15],
-            'name': img_name}
-    print(f'[INFO] the image info path: {image_info_path} has been read out! All image number: {len(IMAGES_INFO)}')
-    return IMAGES_INFO
+def read_cameras_text(path: str):
+    """读取相机参数文本文件,主要包括的相机内参如下CAMERA_ID, WIDTH, 
+    HEIGHT, PIXELSIZE, PARAMS[fx,fy,cx,cy], DISTORTION[K1, K2, K3, P1, P2]"""
+    cams = {}
+    with open(path, 'r') as fid:
+        while True:
+            line = fid.readline()
+            if not line: break
+            line = line.strip()
+            if len(line) > 0 and line[0] != '#':
+                elems = line.split()
+                camera_id = int(elems[0])
+                width = int(elems[1])
+                height = int(elems[2])
+                pixelsize = float(elems[3])
+                params = np.array(tuple(map(float, elems[4:8])))
+                distortion = np.array(tuple(map(float, elems[8:])))
+                cams[camera_id] = Camera(camera_id=camera_id,
+                                        size=(width, height),
+                                        pixelsize=pixelsize,
+                                        focallength=(params[0], params[1]),
+                                        x0y0=(params[2], params[3]),
+                                        distortion=distortion)
+    return cams
 
 
-def read_exr_file(exr_file_path:str)->np.ndarray:
-    """exr_file读取深度图信息,主要包括每个视角的深度图
-    将深度图的exr文件转成后续用于计算的格式,便于操作"""
-    exr_data = pyexr.open(exr_file_path)
-    channels = exr_data.channels()
-    # 获取深度信息
-    depth = exr_data.get()  
-    if depth.ndim == 3: depth = depth[:, :, 0]  
-    return depth.astype(np.float32)
+def read_images_text(path:str):
+    """读取图像参数文本文件,主要包括的图像位姿参数如下IMAGE_ID, CAMERA_ID, 
+    Rwc[9], twc[3], MINDEPTH, MAXDEPTH, NAME"""
+    images = {}
+    with open(path, 'r') as fid:
+        while True:
+            line = fid.readline()
+            if not line: break
+            line = line.strip()
+            if len(line) > 0 and line[0] != '#':
+                elems = line.split()
+                image_id = int(elems[0])
+                Camera_id = int(elems[1])
+                R_matrix = np.array(tuple(map(float, elems[2:11]))).reshape(3, 3)
+                t_matrix = np.array(tuple(map(float, elems[11:14])))
+                depth_range = np.array(tuple(map(float, elems[14:16])))
+                image_name = elems[16]
+                images[image_id] = Photo(image_id=image_id,
+                                         camera_id=Camera_id,
+                                         rotation_matrix=R_matrix,
+                                         project_center=t_matrix,
+                                         depth=(depth_range[0], depth_range[1]),
+                                         name=image_name)
+    return images
 
 
+def read_images_path_text(path:str, replace_dir:bool=True):
+    """读取图像路径文本文件,主要包括IMAGE_ID和IMAGE_PATH
+    replace_dir: 是否替换路径中的某些目录以适应当前环境"""
+    paths_list = {}
+    names_list = {}
+    cluster_list = open(path).read().split()
+    total_num = int(cluster_list[0])
+    for i in range(total_num):
+        index = int (cluster_list[i * 3 + 1])  
+        name = cluster_list[i * 3 + 2]
+        p = cluster_list[i * 3 + 3]
+        if replace_dir:
+            # 修改图像存储路径
+            p = p.split('/')[-4:]
+            path_dir = path.split('/')[:-3]
+            p = os.path.join('/'.join(path_dir), '/'.join(p))
+        paths_list[index] = p
+        names_list[index] = name
+    return paths_list, names_list
+
+
+def read_view_pair_text(pair_path:str, view_num:int):
+    """读取视图对文本文件,主要包括每个样本的视图索引"""
+    metas = []
+    # read the pair file
+    with open(pair_path) as f:
+        num_viewpoint = int(f.readline())
+        # viewpoints
+        for view_idx in range(num_viewpoint):
+            ref_view = [int(f.readline().rstrip())]
+            src_views = [int(x) for x in f.readline().rstrip().split()[1::2]]
+            # filter by no src view and fill to nviews
+            if len(src_views) > 0:
+                if len(src_views) < view_num:
+                    print("{}< num_views:{}".format(len(src_views), view_num))
+                    src_views += [src_views[0]] * (view_num - len(src_views))
+                metas.append(ref_view + src_views)
+    return metas
+
+
+if __name__ == "__main__":
+    pass
