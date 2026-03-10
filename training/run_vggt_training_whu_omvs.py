c0623ac08ee288823768509896cf95edd8589d21
Rtwotwo <744044292@qq.com>
2026-03-10 21:12:08 +0800

    push finished dataloader for training

--- training/run_vggt_training_whu_omvs.py
+++ training/run_vggt_training_whu_omvs.py
@@ -1,6 +1,13 @@
+"""
+Author: Redal
+Date: 2026-03-10
+Description:
+
+Homepage:
+"""
 import torch
 import torch.nn as nn
 from models.vggt.models.vggt import VGGT
-from training.utils.dataloader import WHU_OMVS_Dataset
+from utils.dataset import WHU_OMVS_Dataset
 
 
---
+++ training/utils/data_io.py
@@ -0,0 +1,7 @@
+"""
+Author: Redal
+Date: 2026-03-10
+Description:
+
+Homepage:
+"""
